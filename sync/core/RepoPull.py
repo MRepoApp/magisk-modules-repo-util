66fc3cd2ae24a9fc5a9d4a49b77166e3c8f83029
ya0211 <sanmer@sanmer.dev>
2023-05-12 00:20:31 +0800

    Initialize pull_from_zip method

--- sync/core/RepoPull.py
+++ sync/core/RepoPull.py
@@ -1,22 +1,18 @@
 import os
 import shutil
 from datetime import datetime
-from pathlib import Path
 
 from ..model import *
 from ..utils import Log
 
 
 class RepoPull:
-    def __init__(self, tracks, root_folder, config):
+    def __init__(self, root_folder, config):
         self._log = Log("RepoPull", config.log_dir, config.show_log)
         self._modules_folder = root_folder.joinpath("modules")
         self._local_folder = root_folder.joinpath("local")
-        self.json_file = root_folder.joinpath("json", ModulesJson.filename())
 
-        self._tracks = tracks
-        self._repo_url = config.repo_url
-        self._max_num = config.max_num
+        self._config = config
         self.timestamp = datetime.now().timestamp()
         self.modules_json = ModulesJson(
             name=config.repo_name,
@@ -24,47 +20,48 @@ class RepoPull:
             modules=list()
         )
 
-        self._track_cache = TrackJson()
-
+        self._track = TrackJson.empty()
         self._modules_folder.mkdir(exist_ok=True)
-        self.json_file.parent.mkdir(exist_ok=True)
         self._log.d("__init__")
 
     def __del__(self):
         self._log.d("__del__")
 
+    @staticmethod
+    def _copy_file(old, new, delete_old=True):
+        if not old.samefile(new):
+            shutil.copy(old, new)
+            if delete_old:
+                os.remove(old)
+
     def _get_file_url(self, module_id, file):
         module_folder = self._modules_folder.joinpath(module_id)
-        url = f"{self._repo_url}{self._modules_folder.name}/{module_id}/{file.name}"
+        url = f"{self._config.repo_url}{self._modules_folder.name}/{module_id}/{file.name}"
 
-        if not file.is_relative_to(module_folder):
-            raise FileNotFoundError(f"{file} not in {module_folder}")
+        if not (file.is_relative_to(module_folder) and file.exists()):
+            raise FileNotFoundError(f"{file} is not in {module_folder}")
         else:
             return url
 
-    def _pull_from_zip_common(self, local_module, zip_file, changelog, *, delete_tmp=True):
-        online_module = OnlineModule(local_module)
-        module_folder = self._modules_folder.joinpath(local_module.id)
+    def _pull_from_zip_common(self, zip_file, changelog, *, delete_tmp=True):
+        online_module = LocalModule.from_file(zip_file).to_online_module()
+        module_folder = self._modules_folder.joinpath(online_module.id)
 
         target_zip_file = module_folder.joinpath(online_module.zipfile_filename)
+        if not target_zip_file.exists():
+            self._copy_file(zip_file, target_zip_file, delete_tmp)
+        else:
+            return None
+
         target_changelog = module_folder.joinpath(online_module.changelog_filename)
-
-        if not zip_file.samefile(target_zip_file):
-            shutil.copy(zip_file, target_zip_file)
-            if delete_tmp:
-                os.remove(zip_file)
-
         changelog_url = ""
-        if not (changelog is None or changelog.samefile(target_changelog)):
-            shutil.copy(changelog, target_changelog)
-            if delete_tmp:
-                os.remove(changelog)
-
-            changelog_url = self._get_file_url(local_module.id, target_changelog)
+        if changelog is not None:
+            self._copy_file(changelog, target_changelog, delete_tmp)
+            changelog_url = self._get_file_url(online_module.id, target_changelog)
 
         online_module.license = self._track.license
         online_module.states = AttrDict(
-            zipUrl=self._get_file_url(local_module.id, target_zip_file),
+            zipUrl=self._get_file_url(online_module.id, target_zip_file),
             changelog=changelog_url
         )
 
@@ -81,13 +78,15 @@ class RepoPull:
 
     def pull_from_zip(self, track):
         zip_file = self._local_folder.joinpath(track.update_to)
-        # from local copy to modules
 
-        if type(track.changelog) is str and track.changelog.endswith("md"):
+        if isinstance(track.changelog, str) and track.changelog.endswith("md"):
             changelog = self._local_folder.joinpath(track.changelog)
         else:
             self._log.w(f"pull_from_zip: [{track.id}] -> unsupported changelog type [{track.changelog}]")
             changelog = None
+
+        self._track = track
+        return self._pull_from_zip_common(zip_file, changelog, delete_tmp=False)
 
     def pull_from_track(self, track):
         if track.update_to.startswith("http"):
@@ -104,5 +103,8 @@ class RepoPull:
         self._log.e(f"pull_from_track: [{track.id}] -> unsupported update_to type [{track.update_to}]")
         return None
 
-    def pull_all(self):
+    def pull_module(self, track):
+        pass
+
+    def pull_modules(self, tracks):
         pass
