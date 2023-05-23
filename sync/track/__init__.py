420c96bac195b1de3f3b9f2eb602397288d60eeb
ya0211 <sanmer@sanmer.dev>
2023-05-23 20:39:38 +0800

    Import classes into __init__.py

---
+++ sync/track/__init__.py
@@ -0,0 +1,9 @@
+from .BaseTracks import BaseTracks
+from .GithubTracks import GithubTracks
+from .LocalTracks import LocalTracks
+
+__all__ = [
+    "BaseTracks",
+    "GithubTracks",
+    "LocalTracks"
+]
