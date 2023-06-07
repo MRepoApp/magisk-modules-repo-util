287b1c83e406ce3caaab948b924ef1510ac89266
ya0211 <sanmer@sanmer.dev>
2023-06-07 23:35:09 +0800

    Safely import GithubTracks

--- sync/track/__init__.py
+++ sync/track/__init__.py
@@ -1,6 +1,11 @@
 from .BaseTracks import BaseTracks
-from .GithubTracks import GithubTracks
 from .LocalTracks import LocalTracks
+
+try:
+    from .GithubTracks import GithubTracks
+except ImportError:
+    from .BaseTracks import BaseTracks as GithubTracks
+
 
 __all__ = [
     "BaseTracks",
