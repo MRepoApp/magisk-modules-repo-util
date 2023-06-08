5e2d169e46037ba0de021592292e323f7ac062eb
ya0211 <sanmer@sanmer.dev>
2023-06-08 13:28:47 +0800

    Implement methods in BaseTracks

--- sync/track/BaseTracks.py
+++ sync/track/BaseTracks.py
@@ -1,2 +1,17 @@
 class BaseTracks:
-    pass
+    def __init__(self,  *args, **kwargs):
+        pass
+
+    def get_track(self, *args, **kwargs):
+        pass
+
+    def get_tracks(self, *args, **kwargs):
+        pass
+
+    @property
+    def size(self):
+        return 0
+
+    @property
+    def tracks(self):
+        return list()
