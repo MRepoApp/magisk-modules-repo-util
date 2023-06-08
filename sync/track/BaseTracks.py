8f5c1c8ad53ba8ae54101a10ed3f461c8fd6a775
ya0211 <sanmer@sanmer.dev>
2023-06-08 21:01:04 +0800

    Revert "Implement methods in BaseTracks" This reverts commit 5e2d169e46037ba0de021592292e323f7ac062eb.

--- sync/track/BaseTracks.py
+++ sync/track/BaseTracks.py
@@ -1,17 +1,2 @@
 class BaseTracks:
-    def __init__(self,  *args, **kwargs):
-        pass
-
-    def get_track(self, *args, **kwargs):
-        pass
-
-    def get_tracks(self, *args, **kwargs):
-        pass
-
-    @property
-    def size(self):
-        return 0
-
-    @property
-    def tracks(self):
-        return list()
+    pass
