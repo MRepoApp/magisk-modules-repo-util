a4cb426e59952bf1d58749604d5b02a0f54d2f0b
ya0211 <sanmer@sanmer.dev>
2023-06-11 21:32:31 +0800

    Clean up unused code

--- sync/error/__init__.py
+++ sync/error/__init__.py
@@ -1,9 +1,7 @@
 from .ConfigError import ConfigError
 from .MagiskModuleError import MagiskModuleError
-from .UpdateJsonError import UpdateJsonError
 
 __all__ = [
     "ConfigError",
-    "MagiskModuleError",
-    "UpdateJsonError"
+    "MagiskModuleError"
 ]
--- sync/utils/__init__.py
+++ sync/utils/__init__.py
@@ -1,15 +1,11 @@
 from .GitUtils import GitUtils
 from .HttpUtils import HttpUtils
-from .Input import Input
 from .Log import Log
-from .Print import Print
 from .StrUtils import StrUtils
 
 __all__ = [
     "GitUtils",
     "HttpUtils",
-    "Input",
     "Log",
-    "Print",
     "StrUtils"
 ]
