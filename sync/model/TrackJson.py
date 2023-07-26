d6d5182e83c772ee24ea0d371682a23b5a3e3946
ya0211 <sanmer@sanmer.dev>
2023-07-26 13:04:56 +0800

    Add fields to TrackJson

--- sync/model/TrackJson.py
+++ sync/model/TrackJson.py
@@ -3,6 +3,15 @@ from .JsonIO import JsonIO
 
 
 class TrackJson(AttrDict, JsonIO):
+    # noinspection PyTypeChecker
+    def write(self, file):
+        new = AttrDict()
+        for key in self.expected_fields():
+            if key in self.__dict__:
+                new[key] = self.get(key)
+
+        JsonIO.write(new, file)
+
     @classmethod
     def load(cls, file):
         obj = JsonIO.load(file)
@@ -11,3 +20,19 @@ class TrackJson(AttrDict, JsonIO):
     @classmethod
     def filename(cls):
         return "track.json"
+
+    @classmethod
+    def expected_fields(cls):
+        return [
+            "id",
+            "update_to",
+            "license",
+            "changelog",
+            "website",
+            "source",
+            "tracker",
+            "donate",
+            "added",
+            "last_update",
+            "versions"
+        ]
--- sync/model/TrackJson.pyi
+++ sync/model/TrackJson.pyi
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import List
 
 from .AttrDict import AttrDict
 from .JsonIO import JsonIO
@@ -9,12 +10,19 @@ class TrackJson(AttrDict, JsonIO):
     update_to: str
     license: str
     changelog: str
+    website: str
+    source: str
+    tracker: str
+    donate: str
     added: float
     last_update: float
     versions: int
 
+    def write(self, file: Path): ...
     @classmethod
     def load(cls, file: Path) -> TrackJson: ...
     @classmethod
     def filename(cls) -> str: ...
+    @classmethod
+    def expected_fields(cls) -> List[str]: ...
 
